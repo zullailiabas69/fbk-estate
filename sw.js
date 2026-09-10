46f1fa5a926183e7930073e4efdbf2707f8b5f55
zullailiabas69 <zullailiabas69@gmail.com>
2026-09-10 15:57:54 +0800

    Update service worker for v4.2

--- sw.js
+++ sw.js
@@ -1,37 +1,13 @@
-const CACHE='fbk-estate-v41';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
-  const url=new URL(req.url);
-
-  if(url.origin===self.location.origin){
-    event.respondWith(
-      caches.match(req).then(cached=>cached||fetch(req).then(resp=>{
-        const copy=resp.clone();
-        caches.open(CACHE).then(c=>c.put(req,copy));
-        return resp;
-      }).catch(()=>caches.match('./index.html')))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(req).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
-      return resp;
-    }).catch(()=>caches.match(req))
-  );
+const CACHE='fbk-estate-v42';
+const APP=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ const u=new URL(e.request.url);
+ if(u.origin===self.location.origin){
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./index.html'))));
+ } else {
+  e.respondWith(fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return resp;}).catch(()=>caches.match(e.request)));
+ }
 });
